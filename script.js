1519d2c6421260205bf5c908f6f45dbd2cde2bf6
Pau Joan <pregonipaw@hotmail.com>
2020-03-09 17:27:52 +0100

    Agregado archivo externo css

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const celeste = document.getElementById('celeste')
 const violeta = document.getElementById('violeta')
 const naranja = document.getElementById('naranja')
 const verde = document.getElementById('verde')
-
 const btnEmpezar = document.getElementById('btnEmpezar')
 const ULTIMO_NIVEL = 3
 
